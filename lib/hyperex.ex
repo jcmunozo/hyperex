0c4bccde42fb5801f5a4e18667155914af0b63f5
jcmunozo <jcmunozo@unicauca.edu.co>
2024-11-10 23:08:00 -0500

    Added basic code

---
+++ lib/hyperex.ex
@@ -0,0 +1,18 @@
+defmodule Hyperex do
+  @moduledoc """
+  Documentation for `Hyperex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hyperex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hyperex_test.exs
@@ -0,0 +1,8 @@
+defmodule HyperexTest do
+  use ExUnit.Case
+  doctest Hyperex
+
+  test "greets the world" do
+    assert Hyperex.hello() == :world
+  end
+end
